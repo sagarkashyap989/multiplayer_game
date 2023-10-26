6d1e464d09e64c5008077e84f47f60c1f8a152be
sagarkashypa989 <xyzsmsnkashyap9892044279@gmail.com>
2023-10-26 13:08:36 +0530

    added url

--- frontend/index.js
+++ frontend/index.js
@@ -2,7 +2,7 @@ const BG_COLOUR = '#231f20';
 const SNAKE_COLOUR = '#c2c2c2';
 const FOOD_COLOUR = '#e66916';
 
-const socket = io('http://localhost:3000/');
+const socket = io('https://multiplayer-socket.onrender.com/');
 let roomName;
 
 socket.on('init', handleInit);
